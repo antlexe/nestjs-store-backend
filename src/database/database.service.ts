1f5c327149793faa23f7b32383ab98cd06561d60
Alexey <antiporovi4@gmail.com>
2026-01-22 22:12:43 +0300

    add module auth + fix database.service.ts + change src prisma generate

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,8 +1,14 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client/extension';
+import { PrismaClient } from '../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class DatabaseService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    const adapter = new PrismaPg({ url: process.env.DATABASE_URL });
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
